1f5fac63ebf5755b50f00f610e158b269a99cb62
717538 <717538@dpsk12.net>
2020-11-16 23:25:48 -0700

    Update
    Get rid of qrcode

--- Main.js
+++ Main.js
@@ -1,54 +1,47 @@
+// let output = document.getElementById("qrcode");
+// let qrcode = new QRCode(output, "bruh");
 let number = 0;
 HighGoalBtn = document.getElementById("yo");
-
-let output = document.getElementById("qrcode");
-let qrcode = new QRCode(output, "bruh");
+HighGoalBtn.onclick = function () {number++;HighGoalBtn.innerText = number.toString();}
 
 let rqst = new XMLHttpRequest();
 let url = "http://localhost:3000";
 let type = "POST"
 let SendBtn = document.getElementById("send");
 let data = {
-    "Team_Number": "1410",
-    "Auto_Low_Goal_Scored": "1",
-    "Auto_Low_Goal_Missed": "2",
-    "Auto_Mid_Goal_Scored": "3",
-    "Auto_Mid_Goal_Missed": "4",
-    "Auto_High_Goal_Scored": "5",
-    "Auto_High_Goal_Missed": "6",
-    "Auto_Power_Shot_Target": "3",
-    "Auto_Wobble_Correct_Zone": "Yes",
+    Scout_Number: "8",
+    Team_Number: "1410",
+    Auto_Low_Goal_Scored: "1",
+    Auto_Low_Goal_Missed: "2",
+    Auto_Mid_Goal_Scored: "3",
+    Auto_Mid_Goal_Missed: "4",
+    Auto_High_Goal_Scored: "5",
+    Auto_High_Goal_Missed: "6",
+    Auto_Power_Shot_Target: "3",
+    Auto_Wobble_Correct_Zone: "Yes",
 
-    "Teleop_Low_Goal_Scored": "1",
-    "Teleop_Low_Goal_Missed": "2",
-    "Teleop_Mid_Goal_Scored": "3",
-    "Teleop_Mid_Goal_Missed": "4",
-    "Teleop_High_Goal_Scored": "5",
-    "Teleop_High_Goal_Missed": "6",
+    Teleop_Low_Goal_Scored: "1",
+    Teleop_Low_Goal_Missed: "2",
+    Teleop_Mid_Goal_Scored: "3",
+    Teleop_Mid_Goal_Missed: "4",
+    Teleop_High_Goal_Scored: "5",
+    Teleop_High_Goal_Missed: "6",
 
-    "Endgame_Low_Goal_Scored": "1",
-    "Endgame_Low_Goal_Missed": "2",
-    "Endgame_Mid_Goal_Scored": "3",
-    "Endgame_Mid_Goal_Missed": "4",
-    "Endgame_High_Goal_Scored": "5",
-    "Endgame_High_Goal_Missed": "6",
-    "Endgame_Power_Shot_Target": "3",
-    "Endgame_Wobble_Start_Line": "Yes",
-    "Endgame_Wobble_Drop_Zone": "No",
-    "Endgame_Wobble_Rings" : "3",
+    Endgame_Low_Goal_Scored: "1",
+    Endgame_Low_Goal_Missed: "2",
+    Endgame_Mid_Goal_Scored: "3",
+    Endgame_Mid_Goal_Missed: "4",
+    Endgame_High_Goal_Scored: "5",
+    Endgame_High_Goal_Missed: "6",
+    Endgame_Power_Shot_Target: "3",
+    Endgame_Wobble_Start_Line: "Yes",
+    Endgame_Wobble_Drop_Zone: "No",
+    Endgame_Wobble_Rings : "3",
 
-    "Penalty_Major": "2",
-    "Penalty_Minor": "0",
+    Penalty_Major: "2",
+    Penalty_Minor: "0",
 }
 
 rqst.open(type, url, true);
 rqst.setRequestHeader("Content-Type", "application/json");
-
-SendBtn.onclick = function () {
-    rqst.send(JSON.stringify(data));
-}
-
-HighGoalBtn.onclick = function () {
-    number++;
-    HighGoalBtn.innerText = "High Goal: " + number.toString();
-}
+SendBtn.onclick = function () {rqst.send(JSON.stringify(data));}
