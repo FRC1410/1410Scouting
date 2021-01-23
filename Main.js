3e09d2aa59ec2bca87f79aab49651d6d9a388ba0
RowanEklund <717538@dpsk12.net>
2021-01-23 10:53:57 -0700

    Update Main.js

--- Main.js
+++ Main.js
@@ -129,7 +129,7 @@ EWDZBtn.onmousedown = function () {EWDZ = !EWDZ; if (EWDZ) {EWDZBtn.innerText = 
 
 let rqst = new XMLHttpRequest();
 //let url = "http://localhost:80";
-let url = 'https://a2063f08585b.ngrok.io';
+let url = 'https://740fb1e16dca.ngrok.io';
 let type = "POST"
 let SendBtn = document.getElementById("send");
 
