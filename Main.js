5ef0f07d9663c8678aa6150bf54fc7be59caf410
RowanEklund <717538@dpsk12.net>
2021-09-17 21:42:01 -0600

    Added some final formating

--- Main.js
+++ Main.js
@@ -62,7 +62,9 @@ function openTab(evt, tabName) {
 
 function resetVar() {
     AL = false, AL1S = 0, AL2S = 0, ASM = 0, TL1S = 0, TL2S = 0, TSM = 0, RC = false, PC = false, DF = false, Bal = false;
-    
+
+    ALMain.innerText = 'Rotation Control: Failure';
+
     AL1SMain.innerText = 'Level 1 Cells Scored: ' + AL1S.toString();
     AL2SMain.innerText = 'Level 2 Cells Scored: ' + AL2S.toString();
     ASMMain.innerText = 'Shots Missed: ' + ASM.toString();
@@ -71,21 +73,24 @@ function resetVar() {
     TL2SMain.innerText = 'Level 2 Cells Scored: ' + TL2S.toString();
     TSMMain.innerText = 'Shots Missed: ' + TSM.toString();
 
-    RCMain.innerText = 'Rotation Control: Failure';
-    PCMain.innerText = 'Position Control: Failure';
+    RCMain.innerText = 'Rotation Control: Didnt Attempt';
+    PCMain.innerText = 'Position Control: Didnt Attempt';
     DFMain.innerText = 'Dead on Field (Greater than 15 seconds): False';
     BalMain.innerText = 'Balanced: Failure';
 }
 //dealing with all the minus buttons
 AL1SMinus = document.getElementById("Auto L1 Scored Minus"); AL1SMinus.onmousedown = function () {AL1S--; AL1SMain.innerText = 'Level 1 Cells Scored: ' + AL1S.toString();}
-AL2SMinus = document.getElementById("Auto L2 Scored Minus"); AL1SMinus.onmousedown = function () {AL2S--; AL2SMain.innerText = 'Level 2 Cells Scored: ' + AL2S.toString();}
+AL2SMinus = document.getElementById("Auto L2 Scored Minus"); AL2SMinus.onmousedown = function () {AL2S--; AL2SMain.innerText = 'Level 2 Cells Scored: ' + AL2S.toString();}
 ASMMinus = document.getElementById("Auto Shots Missed Minus"); ASMMinus.onmousedown = function () {ASM--; ASMMain.innerText = 'Shots Missed: ' + ASM.toString();}
 
 TL1SMinus = document.getElementById("Teleop L1 Scored Minus"); TL1SMinus.onmousedown = function () {TL1S--; TL1SMain.innerText = 'Level 1 Cells Scored: ' + TL1S.toString();}
-TL2SMinus = document.getElementById("Teleop L2 Scored Minus"); TL1SMinus.onmousedown = function () {TL2S--; TL2SMain.innerText = 'Level 2 Cells Scored: ' + TL2S.toString();}
+TL2SMinus = document.getElementById("Teleop L2 Scored Minus"); TL2SMinus.onmousedown = function () {TL2S--; TL2SMain.innerText = 'Level 2 Cells Scored: ' + TL2S.toString();}
 TSMMinus = document.getElementById("Teleop Shots Missed Minus"); TSMMinus.onmousedown = function () {TSM--; TSMMain.innerText = 'Shots Missed: ' + TSM.toString();}
 
 //add functionality to the buttons
+ALMain = document.getElementById("Auto Line");
+ALMain.onmousedown = function () {AL = !AL; if (AL) {ALMain.innerText = 'Auto Line: Success';} else {ALMain.innerText = 'Auto Line: Failure';}}
+
 AL1SMain = document.getElementById("Auto L1 Scored");
 AL1SMain.onmousedown = function () {if (event.buttons === 2 || event.buttons === 4) {AL1S--;} else {AL1S++;}AL1SMain.innerText = 'Level 1 Cells Scored: ' + AL1S.toString();}
 AL2SMain = document.getElementById("Auto L2 Scored");
@@ -101,9 +106,9 @@ TSMMain = document.getElementById("Teleop Shots Missed");
 TSMMain.onmousedown = function () {if (event.buttons === 2 || event.buttons === 4) {TSM--;} else {TSM++;}TSMMain.innerText = 'Shots Missed: ' + TSM.toString();}
 
 RCMain = document.getElementById("Rotation");
-RCMain.onmousedown = function () {RC = !RC; if (RC) {RCMain.innerText = 'Rotation Control: Success';} else {RCMain.innerText = 'Rotation Control: Failure';}}
+RCMain.onmousedown = function () {RC = !RC; if (RC) {RCMain.innerText = 'Rotation Control: Attempted';} else {RCMain.innerText = 'Rotation Control: Didnt Attempt';}}
 PCMain = document.getElementById("Position");
-PCMain.onmousedown = function () {PC = !PC; if (PC) {PCMain.innerText = 'Position Control: Success';} else {PCMain.innerText = 'Position Control: Failure';}}
+PCMain.onmousedown = function () {PC = !PC; if (PC) {PCMain.innerText = 'Position Control: Attempted';} else {PCMain.innerText = 'Position Control: Didnt Attempt';}}
 DFMain = document.getElementById("Dead on Field");
 DFMain.onmousedown = function () {DF = !DF; if (DF) {DFMain.innerText = 'Dead on Field (Greater than 15 seconds): True';} else {DFMain.innerText = 'Dead on Field (Greater than 15 seconds): False';}}
 
