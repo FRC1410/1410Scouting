07de7bfb9bf9db7fe04c601bceb4c8aa62c191d2
Rowan Eklund <717538@dpsk12.net>
2022-01-08 19:31:33 -0700

    Fixed the stupid server

--- components/Send.js
+++ components/Send.js
@@ -1,13 +1,12 @@
 import {data} from "../helpers/data"
 import Button from "@mui/material/Button"
-import Popup from "reactjs-popup"
 
 function send() {
     let req = new XMLHttpRequest();
     try {
-        req.open('Post', 'localhost:3800', true)
+        req.open('POST', 'http://localhost:1800', true)
         req.setRequestHeader('Content-Type', 'application/json')
-        req.send(data)
+        req.send(JSON.stringify(data))
         alert('Data sent!')
     } catch (e) {
         console.error(e)
@@ -18,8 +17,5 @@ function send() {
 export default function Send() {
     return (
         <Button className='Send' variant='contained' onClick={send} disableRipple fullWidth disableElevation>Send Scouting Data</Button>
-        // <Popup trigger={<Button className='Send' variant='contained' onClick={() => {console.log('test3'); send()}} disableRipple fullWidth disableElevation>Send Scouting Data</Button>} position='bottom center' closeOnDocumentClick>
-        //     <h1>Data Sent!</h1>
-        // </Popup>
     )
 }
