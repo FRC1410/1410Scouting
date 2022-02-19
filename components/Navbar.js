e093e9e86a20b6c289201eb93038071836a391b1
Rowan Eklund <717538@dpsk12.net>
2022-02-18 22:11:35 -0700

    feat(Navbar): Made it better

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,40 +1,27 @@
-// import Link from "next/link"
-//
-// export default function Navbar() {
-//     return (
-//         <nav>
-//             <Link href='/tabs/pregame'><a>Pre-Game</a></Link>
-//             <Link href='/tabs/auto'><a>Auto</a></Link>
-//             <Link href='/tabs/teleop'><a>Teleop</a></Link>
-//             <Link href='/tabs/endgame'><a>Endgame</a></Link>
-//             <Link href='/tabs/send'><a>Send</a></Link>
-//         </nav>
-//     )
-// }
 import { useState } from 'react'
 
-function NavLink({ to, children }) {
-    return <a href={to} className='mx-4 text-white'>
+function NavLink({ to, children, open }) {
+    return <a href={to} className={`mx-4 text-white ${open ? 'bg-teal-700' : ''}`}>
         { children }
     </a>
 }
 function MobileNav({ open, setOpen }) {
     return (
-        <div className={`absolute top-0 left-0 h-screen w-screen bg-teal-400 transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md `}>
+        <div className={`absolute top-0 left-0 h-screen w-screen bg-teal-400 transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md`}>
             <div className="flex flex-col ml-4">
-                <a className="text-xl font-medium my-4" href="/pages/tabs/pregame.js" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
+                <a className="text-xl font-medium my-4 text-white" href="/tabs/pregame" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
                     Pregame
                 </a>
-                <a className="text-xl font-medium my-4" href="/pages/tabs/auto.js" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
+                <a className="text-xl font-medium my-4 text-white" href="/tabs/auto" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
                     Auto
                 </a>
-                <a className="text-xl font-medium my-4" href="/pages/tabs/teleop.js" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
+                <a className="text-xl font-medium my-4 text-white" href="/tabs/teleop" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
                     Teleop
                 </a>
-                <a className="text-xl font-medium my-4" href="/pages/tabs/endgame.js" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
+                <a className="text-xl font-medium my-4 text-white" href="/tabs/endgame" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
                     Endgame
                 </a>
-                <a className="text-xl font-medium my-4" href="/pages/tabs/send.js" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
+                <a className="text-xl font-medium my-4 text-white" href="/tabs/send" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
                     Send
                 </a>
             </div>
@@ -44,8 +31,10 @@ function MobileNav({ open, setOpen }) {
 
 export default function Navbar() {
     const [open, setOpen] = useState(false)
+    const [selected, setSelected] = useState(false)
+
     return (
-        <nav className="flex filter drop-shadow-md bg-teal-400 px-4 py-4 h-20 items-center">
+        <nav className="flex filter drop-shadow-md bg-teal-400 px-4 py-4 h-14 items-center">
             <MobileNav open={open} setOpen={setOpen}/>
             <div className="w-9/12 flex justify-end items-center">
 
@@ -59,7 +48,7 @@ export default function Navbar() {
                 </div>
 
                 <div className="hidden md:flex">
-                    <NavLink to="/tabs/pregame">
+                    <NavLink to="/tabs/pregame" >
                         PREGAME
                     </NavLink>
                     <NavLink to="/tabs/auto">
