a3e22a700ae3e580fb316c632f919a79c0878fb4
Rowan Eklund <717538@dpsk12.net>
2022-02-20 00:41:42 -0700

    feat(Teleop): Added new endgame scheme

--- pages/tabs/endgame.js
+++ pages/tabs/endgame.js
@@ -6,9 +6,24 @@ export default function Endgame() {
     return (
         <>
             <Navbar page="Endgame" />
-            <div className='Page'>
-                <Dropdown onChange={(value) => data.endgame.var1 = value.value} options={['Climbed', 'Parked', 'None of the Above']} placeholder="Select an Endgame State" />
-                <Dropdown onChange={(value) => data.endgame.var2 = value.value} options={['Balanced', 'Did Not Balance']} placeholder="Select a Balancing State" />
+            <div className="flex justify-center py-4">
+                <button className={"w-5/6 py-2 rounded text-white text-3xl font-bold hover:bg-blue-200 " + (data.endgame.var1 ? "bg-blue-400 active:bg-blue-500" : "bg-blue-500 active:bg-blue-400")} onClick={() => data.endgame.var1 = !data.endgame.var1}>No Climb Attempted</button>
+            </div>
+            <div className="flex justify-center py-4">
+                <button className={"w-5/6 py-2 rounded text-white text-3xl font-bold hover:bg-blue-200 " + (data.endgame.var2 ? "bg-blue-400 active:bg-blue-500" : "bg-blue-500 active:bg-blue-400")} onClick={() => data.endgame.var2 = !data.endgame.var2}>Climb Failed</button>
+            </div>
+            <div className="flex justify-center py-4">
+                <button className={"w-5/6 py-2 rounded text-white text-3xl font-bold hover:bg-blue-200 " + (data.endgame.var3 ? "bg-blue-400 active:bg-blue-500" : "bg-blue-500 active:bg-blue-400")} onClick={() => data.endgame.var3 = !data.endgame.var3}>Climbed Successfully</button>
+            </div>
+            <div>
+                <div className={"flex flex-row justify-evenly py-4 " + (data.endgame.var3 ? "visible" : "invisible")}>
+                    <button className={"basis-1/3 py-2 rounded text-white text-2xl font-bold hover:bg-blue-200 " + (data.endgame.var4 === "Low" ? "bg-blue-400 active:bg-blue-500" : "bg-blue-500 active:bg-blue-400")} onClick={() => data.endgame.var4 = "Low"}>Low</button>
+                    <button className={"basis-1/3 py-2 rounded text-white text-2xl font-bold hover:bg-blue-200 " + (data.endgame.var4 === "Mid" ? "bg-blue-400 active:bg-blue-500" : "bg-blue-500 active:bg-blue-400")} onClick={() => data.endgame.var4 = "Mid"}>Mid</button>
+                </div>
+                <div className={"flex flex-row justify-evenly " + (data.endgame.var3 ? "visible" : "invisible")}>
+                    <button className={"basis-1/3 py-2 rounded text-white text-2xl font-bold hover:bg-blue-200 " + (data.endgame.var4 === "High" ? "bg-blue-400 active:bg-blue-500" : "bg-blue-500 active:bg-blue-400")} onClick={() => data.endgame.var4 = "High"}>High</button>
+                    <button className={"basis-1/3 py-2 rounded text-white text-2xl font-bold hover:bg-blue-200 " + (data.endgame.var4 === "Traversal" ? "bg-blue-400 active:bg-blue-500" : "bg-blue-500 active:bg-blue-400")} onClick={() => data.endgame.var4 = "Traversal"}>Traversal</button>
+                </div>
             </div>
         </>
     )
