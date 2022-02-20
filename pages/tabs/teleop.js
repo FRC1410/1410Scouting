ca09a4522fdf1ac9071be018b154de3ad4e3ec7d
Rowan Eklund <717538@dpsk12.net>
2022-02-20 00:23:00 -0700

    feat(Teleop): Added new teleop scheme

--- pages/tabs/teleop.js
+++ pages/tabs/teleop.js
@@ -8,22 +8,31 @@ export default function Teleop() {
     return (
         <>
             <Navbar page="Teleop" />
-            <div className='Page'>
-                <ButtonGroup variant='contained' color='primary' aria-label='split button' disableRipple fullWidth disableElevation>
-                    <Button id='1' onClick={() => {data.teleop.var1++; document.getElementById('1').innerText=`Level 1 Cells Scored: ${data.teleop.var1}`}}>Level 1 Cells Scored</Button>
-                    <Button style={{width: 100}} onClick={() => {data.teleop.var1--; document.getElementById('1').innerText=`Level 1 Cells Scored: ${data.teleop.var1}`}}>-</Button>
-                </ButtonGroup>
-                <ButtonGroup variant='contained' color='primary' aria-label='split button' disableRipple fullWidth disableElevation>
-                    <Button id='2' onClick={() => {data.teleop.var2++; document.getElementById('2').innerText=`Level 2 Cells Scored: ${data.teleop.var2}`}}>Level 2 Cells Scored</Button>
-                    <Button style={{width: 100}} onClick={() => {data.teleop.var2--; document.getElementById('2').innerText=`Level 2 Cells Scored: ${data.teleop.var2}`}}>-</Button>
-                </ButtonGroup>
-                <ButtonGroup variant='contained' color='secondary' aria-label='split button' disableRipple fullWidth disableElevation>
-                    <Button id='3' className='Missed' onClick={() => {data.teleop.var3++; document.getElementById('3').innerText=`Shots Missed: ${data.teleop.var3}`}}>Shots Missed</Button>
-                    <Button className='Missed' style={{width: 100}} onClick={() => {data.teleop.var3--; document.getElementById('3').innerText=`Shots Missed: ${data.teleop.var3}`}}>-</Button>
-                </ButtonGroup>
-                <Dropdown onChange={(value) => data.teleop.var4 = value.value} options={['Completed', 'Did Not Complete']} placeholder="Select a Rotation Control State" />
-                <Dropdown onChange={(value) => data.teleop.var5 = value.value} options={['Completed', 'Did Not Complete']} placeholder="Select a Position Control State" />
-                <Dropdown onChange={(value) => data.teleop.var6 = value.value} options={['Dead', 'Not Dead']} placeholder="Select a Dead on Field State" />
+            <h1 className="flex justify-center py-2 text-white text-3xl font-bold">Low Hub Scored</h1>
+            <div className="flex flex-row justify-evenly pb-5">
+                <button className="basis-1/4 rounded-full bg-blue-500 text-white text-7xl font-bold hover:bg-blue-200 active:bg-blue-400" onClick={() => data.teleop.var1++}>+</button>
+                <h1 className="basis-1/5 text-center text-white text-8xl font-bold">{data.teleop.var1}</h1>
+                <button className="basis-1/4 rounded-full bg-blue-500 text-white text-7xl font-bold hover:bg-blue-200 active:bg-blue-400" onClick={() => data.teleop.var1--}>-</button>
+            </div>
+            <h1 className="flex justify-center py-2 text-white text-3xl font-bold">High Hub Scored</h1>
+            <div className="flex flex-row justify-evenly pb-5">
+                <button className="basis-1/4 rounded-full bg-blue-500 text-white text-7xl font-bold hover:bg-blue-200 active:bg-blue-400" onClick={() => data.teleop.var2++}>+</button>
+                <h1 className="basis-1/5 text-center text-white text-8xl font-bold">{data.teleop.var2}</h1>
+                <button className="basis-1/4 rounded-full bg-blue-500 text-white text-7xl font-bold hover:bg-blue-200 active:bg-blue-400" onClick={() => data.teleop.var2--}>-</button>
+            </div>
+            <h1 className="flex justify-center py-2 text-white text-3xl font-bold">Shots Missed</h1>
+            <div className="flex flex-row justify-evenly pb-5">
+                <button className="basis-1/4 rounded-full bg-blue-500 text-white text-7xl font-bold hover:bg-blue-200 active:bg-blue-400" onClick={() => data.teleop.var3++}>+</button>
+                <h1 className="basis-1/5 text-center text-white text-8xl font-bold">{data.teleop.var3}</h1>
+                <button className="basis-1/4 rounded-full bg-blue-500 text-white text-7xl font-bold hover:bg-blue-200 active:bg-blue-400" onClick={() => data.teleop.var3--}>-</button>
+            </div>
+            <div className="flex flex-row justify-evenly py-4">
+                <button className={"basis-1/3 py-2 rounded text-white text-2xl font-bold hover:bg-blue-200 " + (data.teleop.var4 ? "bg-blue-400 active:bg-blue-500" : "bg-blue-500 active:bg-blue-400")} onClick={() => data.teleop.var4 = !data.teleop.var4}>Played Defense</button>
+                <button className={"basis-1/3 py-2 rounded text-white text-2xl font-bold hover:bg-blue-200 " + (data.teleop.var5 ? "bg-blue-400 active:bg-blue-500" : "bg-blue-500 active:bg-blue-400")} onClick={() => data.teleop.var5 = !data.teleop.var5}>Was Defended</button>
+            </div>
+            <div className="flex flex-row justify-evenly py-4">
+                <button className={"basis-1/3 py-2 rounded text-white text-2xl font-bold hover:bg-blue-200 " + (data.teleop.var6 ? "bg-blue-400 active:bg-blue-500" : "bg-blue-500 active:bg-blue-400")} onClick={() => data.teleop.var6 = !data.teleop.var6}>+15 sec Dead on Field</button>
+                <button className={"basis-1/3 py-2 rounded text-white text-2xl font-bold hover:bg-blue-200 " + (data.teleop.var7 ? "bg-blue-400 active:bg-blue-500" : "bg-blue-500 active:bg-blue-400")} onClick={() => data.teleop.var7 = !data.teleop.var7}>2+ Fouls</button>
             </div>
         </>
     )
