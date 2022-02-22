88fd7283e3d097c4142dfd99bbd6837c2f97dbb2
Rowan Eklund <717538@dpsk12.net>
2022-02-21 18:20:05 -0700

    style(*): Changed more styling to meet Sophia's design "recommendations"

--- pages/tabs/auto.js
+++ pages/tabs/auto.js
@@ -15,21 +15,21 @@ export default function Auto() {
             </div>
             <h1 className="flex justify-center py-2 text-white text-3xl font-bold">Low Hub Scored</h1>
             <div className="flex flex-row justify-evenly pb-5">
-                <button className="basis-1/4 rounded-full bg-blue-500 text-white text-7xl font-bold hover:bg-blue-200 active:bg-blue-400" onClick={() => {setData("auto", "var2", getData("auto", "var2") - 1); forceUpdate()}}>-</button>
+                <button className="basis-1/4 rounded-full bg-emerald-600 text-white text-7xl font-bold hover:bg-emerald-200 active:bg-teal-600" onClick={() => {setData("auto", "var2", getData("auto", "var2") - 1); forceUpdate()}}>-</button>
                 <h1 className="basis-1/5 text-center text-white text-8xl font-bold">{getData("auto", "var2")}</h1>
-                <button className="basis-1/4 rounded-full bg-blue-500 text-white text-7xl font-bold hover:bg-blue-200 active:bg-blue-400" onClick={() => {setData("auto", "var2", getData("auto", "var2") + 1); forceUpdate()}}>+</button>
+                <button className="basis-1/4 rounded-full bg-emerald-600 text-white text-7xl font-bold hover:bg-emerald-200 active:bg-teal-600" onClick={() => {setData("auto", "var2", getData("auto", "var2") + 1); forceUpdate()}}>+</button>
             </div>
             <h1 className="flex justify-center py-2 text-white text-3xl font-bold">High Hub Scored</h1>
             <div className="flex flex-row justify-evenly pb-5">
-                <button className="basis-1/4 rounded-full bg-blue-500 text-white text-7xl font-bold hover:bg-blue-200 active:bg-blue-400" onClick={() => {setData("auto", "var3", getData("auto", "var3") - 1); forceUpdate()}}>-</button>
+                <button className="basis-1/4 rounded-full bg-emerald-600 text-white text-7xl font-bold hover:bg-emerald-200 active:bg-teal-600" onClick={() => {setData("auto", "var3", getData("auto", "var3") - 1); forceUpdate()}}>-</button>
                 <h1 className="basis-1/5 text-center text-white text-8xl font-bold">{getData("auto", "var3")}</h1>
-                <button className="basis-1/4 rounded-full bg-blue-500 text-white text-7xl font-bold hover:bg-blue-200 active:bg-blue-400" onClick={() => {setData("auto", "var3", getData("auto", "var3") + 1); forceUpdate()}}>+</button>
+                <button className="basis-1/4 rounded-full bg-emerald-600 text-white text-7xl font-bold hover:bg-emerald-200 active:bg-teal-600" onClick={() => {setData("auto", "var3", getData("auto", "var3") + 1); forceUpdate()}}>+</button>
             </div>
             <h1 className="flex justify-center py-2 text-white text-3xl font-bold">Shots Missed</h1>
             <div className="flex flex-row justify-evenly pb-5">
-                <button className="basis-1/4 rounded-full bg-blue-500 text-white text-7xl font-bold hover:bg-blue-200 active:bg-blue-400" onClick={() => {setData("auto", "var4", getData("auto", "var4") - 1); forceUpdate()}}>-</button>
+                <button className="basis-1/4 rounded-full bg-emerald-600 text-white text-7xl font-bold hover:bg-emerald-200 active:bg-teal-600" onClick={() => {setData("auto", "var4", getData("auto", "var4") - 1); forceUpdate()}}>-</button>
                 <h1 className="basis-1/5 text-center text-white text-8xl font-bold">{getData("auto", "var4")}</h1>
-                <button className="basis-1/4 rounded-full bg-blue-500 text-white text-7xl font-bold hover:bg-blue-200 active:bg-blue-400" onClick={() => {setData("auto", "var4", getData("auto", "var4") + 1); forceUpdate()}}>+</button>
+                <button className="basis-1/4 rounded-full bg-emerald-600 text-white text-7xl font-bold hover:bg-emerald-200 active:bg-teal-600" onClick={() => {setData("auto", "var4", getData("auto", "var4") + 1); forceUpdate()}}>+</button>
             </div>
         </>
     )
