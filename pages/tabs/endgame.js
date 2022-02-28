c5b7bf214684b76304d75c98d46ed9813910d1db
digabro <diego.olin@gmail.com>
2022-02-28 09:41:42 -0700

    removed hover feature
    replaced 'hover:' with 'backround-color:'
    removed 'test' from config.json

--- pages/tabs/endgame.js
+++ pages/tabs/endgame.js
@@ -9,22 +9,22 @@ export default function Endgame() {
         <>
             <Navbar page="Endgame" />
             <div className="flex justify-center py-4">
-                <button className={"w-5/6 py-2 rounded text-white text-3xl font-bold hover:bg-emerald-200 " + (getData("endgame", "var1") ? "bg-stone-400 active:bg-emerald-600" : "bg-emerald-600 active:bg-stone-400")} onClick={() => {setData("endgame", "var1", !getData("endgame", "var1")); forceUpdate()}}>No Climb Attempted</button>
+                <button className={"w-5/6 py-2 rounded text-white text-3xl font-bold backround-color:bg-emerald-600 " + (getData("endgame", "var1") ? "bg-stone-400 active:bg-emerald-600" : "bg-emerald-600 active:bg-stone-400")} onClick={() => {setData("endgame", "var1", !getData("endgame", "var1")); forceUpdate()}}>No Climb Attempted</button>
             </div>
             <div className="flex justify-center py-4">
-                <button className={"w-5/6 py-2 rounded text-white text-3xl font-bold hover:bg-emerald-200 " + (getData("endgame", "var2") ? "bg-stone-400 active:bg-emerald-600" : "bg-emerald-600 active:bg-stone-400")} onClick={() => {setData("endgame", "var2", !getData("endgame", "var2")); forceUpdate()}}>Climb Failed</button>
+                <button className={"w-5/6 py-2 rounded text-white text-3xl font-bold backround-color:bg-emerald-600 " + (getData("endgame", "var2") ? "bg-stone-400 active:bg-emerald-600" : "bg-emerald-600 active:bg-stone-400")} onClick={() => {setData("endgame", "var2", !getData("endgame", "var2")); forceUpdate()}}>Climb Failed</button>
             </div>
             <div className="flex justify-center py-4">
-                <button className={"w-5/6 py-2 rounded text-white text-3xl font-bold hover:bg-emerald-200 " + (getData("endgame", "var3") ? "bg-stone-400 active:bg-emerald-600" : "bg-emerald-600 active:bg-stone-400")} onClick={() => {setData("endgame", "var3", !getData("endgame", "var3")); forceUpdate()}}>Climbed Successfully</button>
+                <button className={"w-5/6 py-2 rounded text-white text-3xl font-bold backround-color:bg-emerald-600 " + (getData("endgame", "var3") ? "bg-stone-400 active:bg-emerald-600" : "bg-emerald-600 active:bg-stone-400")} onClick={() => {setData("endgame", "var3", !getData("endgame", "var3")); forceUpdate()}}>Climbed Successfully</button>
             </div>
             <div>
                 <div className={"flex flex-row justify-evenly py-4 " + (getData("endgame", "var3") ? "visible" : "invisible")}>
-                    <button className={"basis-1/3 py-2 rounded text-white text-2xl font-bold hover:bg-emerald-200 " + (getData("endgame", "var4") === "Low" ? "bg-stone-400 active:bg-emerald-600" : "bg-emerald-600 active:bg-stone-400")} onClick={() => {setData("endgame", "var4", "Low"); forceUpdate()}}>Low</button>
-                    <button className={"basis-1/3 py-2 rounded text-white text-2xl font-bold hover:bg-emerald-200 " + (getData("endgame", "var4") === "Mid" ? "bg-stone-400 active:bg-emerald-600" : "bg-emerald-600 active:bg-stone-400")} onClick={() => {setData("endgame", "var4", "Mid"); forceUpdate()}}>Mid</button>
+                    <button className={"basis-1/3 py-2 rounded text-white text-2xl font-bold backround-color:bg-emerald-600 " + (getData("endgame", "var4") === "Low" ? "bg-stone-400 active:bg-emerald-600" : "bg-emerald-600 active:bg-stone-400")} onClick={() => {setData("endgame", "var4", "Low"); forceUpdate()}}>Low</button>
+                    <button className={"basis-1/3 py-2 rounded text-white text-2xl font-bold backround-color:bg-emerald-600 " + (getData("endgame", "var4") === "Mid" ? "bg-stone-400 active:bg-emerald-600" : "bg-emerald-600 active:bg-stone-400")} onClick={() => {setData("endgame", "var4", "Mid"); forceUpdate()}}>Mid</button>
                 </div>
                 <div className={"flex flex-row justify-evenly " + (getData("endgame", "var3") ? "visible" : "invisible")}>
-                    <button className={"basis-1/3 py-2 rounded text-white text-2xl font-bold hover:bg-emerald-200 " + (getData("endgame", "var4") === "High" ? "bg-stone-400 active:bg-emerald-600" : "bg-emerald-600 active:bg-stone-400")} onClick={() => {setData("endgame", "var4", "High"); forceUpdate()}}>High</button>
-                    <button className={"basis-1/3 py-2 rounded text-white text-2xl font-bold hover:bg-emerald-200 " + (getData("endgame", "var4") === "Traversal" ? "bg-stone-400 active:bg-emerald-600" : "bg-emerald-600 active:bg-stone-400")} onClick={() => {setData("endgame", "var4", "Traversal"); forceUpdate()}}>Traversal</button>
+                    <button className={"basis-1/3 py-2 rounded text-white text-2xl font-bold backround-color:bg-emerald-600 " + (getData("endgame", "var4") === "High" ? "bg-stone-400 active:bg-emerald-600" : "bg-emerald-600 active:bg-stone-400")} onClick={() => {setData("endgame", "var4", "High"); forceUpdate()}}>High</button>
+                    <button className={"basis-1/3 py-2 rounded text-white text-2xl font-bold backround-color:bg-emerald-600 " + (getData("endgame", "var4") === "Traversal" ? "bg-stone-400 active:bg-emerald-600" : "bg-emerald-600 active:bg-stone-400")} onClick={() => {setData("endgame", "var4", "Traversal"); forceUpdate()}}>Traversal</button>
                 </div>
             </div>
         </>
