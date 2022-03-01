fa77ddea7aa042f134baef39e02033555cda1082
digabro <diego.olin@gmail.com>
2022-03-01 09:25:16 -0700

    Changed color
    Drew wanted the active button color to be violet 600 rather than stone 400

--- components/Reset.js
+++ components/Reset.js
@@ -3,7 +3,7 @@ import {resetData} from "../helpers/data"
 export default function Reset() {
     return (
         <div className="flex justify-center py-4">
-            <button className="w-5/6 py-2 rounded text-white text-3xl font-bold bg-emerald-600 backround-color:bg-emerald-600 active:bg-teal-600" onClick={() => resetData()}>Reset</button>
+            <button className="w-5/6 py-2 rounded text-white text-3xl font-bold bg-emerald-600 backround-color:bg-emerald-600 active:bg-violet-600" onClick={() => resetData()}>Reset</button>
         </div>
     )
 }
--- pages/tabs/pregame.js
+++ pages/tabs/pregame.js
@@ -2,6 +2,7 @@ import Navbar from "../../components/Navbar"
 import Dropdown from "../../components/Dropdown"
 import { useForceUpdate } from "../../helpers/Refresh"
 import { getData, setData } from "/helpers/data"
+import Reset from "../../components/Reset"
 
 export default function Pregame() {
     const forceUpdate = useForceUpdate()
@@ -28,6 +29,9 @@ export default function Pregame() {
             <div className="flex flex-row justify-evenly py-4">
                 <button className={"basis-1/3 py-2 rounded text-white text-7xl font-bold backround-color:bg-red-600 " + (getData("pregame", "alliance_position") === "R3" ? "bg-red-400 active:bg-red-600" : "bg-red-600 active:bg-red-400")} onClick={() => {setData("pregame", "alliance_position", "R3"); forceUpdate()}}>R3</button>
                 <button className={"basis-1/3 py-2 rounded text-white text-7xl font-bold backround-color:bg-blue-600 " + (getData("pregame", "alliance_position") === "B3" ? "bg-blue-400 active:bg-blue-600" : "bg-blue-600 active:bg-blue-400")} onClick={() => {setData("pregame", "alliance_position", "B3"); forceUpdate()}}>B3</button>
+            </div>
+            <div>
+                <Reset />
             </div>
         </>
     )
