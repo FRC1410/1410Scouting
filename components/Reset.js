22fe6f144032ed313912a256958124f155cdaa9e
digabro <diego.olin@gmail.com>
2022-03-01 16:33:13 -0700

    reset redirects to pregame
    fixed it so it works with all webpages

--- components/Reset.js
+++ components/Reset.js
@@ -1,9 +1,21 @@
 import {resetData} from "../helpers/data"
 
+function returnPregame(){
+    var newPagefrc = '/scouting/tabs/pregame' 
+    var newPagelocal = '/tabs/pregame'
+    var ogPage = location.origin
+    if (location.origin == 'frc1410.org'){
+        window.location.replace(ogPage + newPagefrc)
+    }
+    else{
+        window.location.replace(ogPage + newPagelocal)
+    }
+}
+
 export default function Reset() {
     return (
         <div className="flex justify-center py-4">
-            <button className="w-5/6 py-2 rounded text-white text-3xl font-bold bg-emerald-600 backround-color:bg-emerald-600 active:bg-violet-600" onClick={() => { resetData(); window.location.assign("http://frc1410.org/scouting/tabs/pregame"); }}>Reset</button>
+            <button className="w-5/6 py-2 rounded text-white text-3xl font-bold bg-emerald-600 backround-color:bg-emerald-600 active:bg-violet-600" onClick={() => { resetData(); returnPregame(); }}>Reset</button>
         </div>
     )
 }
