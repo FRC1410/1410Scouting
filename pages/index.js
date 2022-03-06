c8c2108365f0caae1b0d4c5c8cd4c51eeef0f43a
Rowan Eklund <717538@dpsk12.net>
2022-03-06 12:54:42 -0700

    fix(Favicon): Reenabled favicon icon

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
         <Head>
             <title>1410 Match Scouting</title>
             <meta name="description" content="A data collection app used by the frc 1410 scouting sub-team" />
-            <link rel="icon" href="/public/favicon.png" />
+            <link rel="icon" href="./favicon.png" />
         </Head>
     </div>
   )
