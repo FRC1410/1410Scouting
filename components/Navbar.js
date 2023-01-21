5f307a4797b0dbfa9516c4bafda5a3df3f52634b
SirKelvin4 <93285266+SirKelvin4@users.noreply.github.com>
2023-01-20 20:11:55 -0700

    2023 Scouting Updates

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,24 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 
 const navPrimary = "bg-green-700"
 const navSecondary = "bg-green-900"
+
+const PageName = [
+    { name: 'pregame', reference: '/tabs/pregame' },
+    { name: 'auto', reference: '/tabs/auto'}, 
+    { name: 'teleop', reference: '/tabs/teleop' },
+    { name: 'send', reference: '/tabs/send'}
+]
 
 function MobileNav({ open, setOpen, page }) {
     return (
         <div className={`absolute top-0 left-0 h-screen w-screen ${navPrimary} transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md`}>
             <div className="flex flex-col ml-4">
-                <a className={"rounded w-1/4 text-xl font-medium my-4 text-white text-center " + (page === "Pregame" && navSecondary)} href="/tabs/pregame" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
-                    PREGAME
+                <a className={"rounded w-1/4 text-white text-center text-x1 font-medium my-4 " + (page === PageName[PageNum].name && navSecondary)} href="/tabs/pregame" onClick={(() => setTimeout(() => {setOpen(!open)}, 100))}>
+                    BACK
+                </a>
+                <a className={"rounded w-1/4 text-xl font-medium my-4 text-white text-center " + (page === PageName[PageNum].name && navSecondary)} href="/tabs/pregame" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
+                    Back
                 </a>
                 <a className={"rounded w-1/4 text-xl font-medium my-4 text-white text-center " + (page === "Auto" && navSecondary)} href="/tabs/auto" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
                     AUTO
@@ -33,8 +43,8 @@ export default function Navbar({ page }) {
     return (
         <nav className={`flex filter drop-shadow-md ${navPrimary} px-4 py-8 h-14 my-2 items-center justify-evenly`}>
             <MobileNav open={open} setOpen={setOpen} page={page} />
-            <div className="w-9/12 flex justify-end items-center">
-
+            <div className="w-9/12 flex justify-center items-center">
+                </div>
                 <div className="z-50 flex relative w-8 h-8 flex-col justify-between items-center md:hidden" onClick={() => {
                     setOpen(!open)
                 }}>
@@ -45,23 +55,13 @@ export default function Navbar({ page }) {
                 </div>
 
                 <div className="hidden md:flex">
-                    <a href="/tabs/pregame" className={"mx-4 rounded text-white px-3 " + (page === "Pregame" && navSecondary)}>
-                        PREGAME
+                    <a onClick={setPageNum(pageNum)} href={PageName[pageNum].reference} className={"basis-1/3 rounded text-white px-3 " + (page === PageName[pageNum].name && navSecondary)}>
+                        Back
                     </a>
-                    <a href="/tabs/auto" className={"mx-4 rounded text-white px-3 " + (page === "Auto" && navSecondary)}>
-                        AUTO
-                    </a>
-                    <a href="/tabs/teleop" className={"mx-4 rounded text-white px-3 " + (page === "Teleop" && navSecondary)}>
-                        TELEOP
-                    </a>
-                    <a href="/tabs/endgame" className={"mx-4 rounded text-white px-3 " + (page === "Endgame" && navSecondary)}>
-                        ENDGAME
-                    </a>
-                    <a href="/tabs/send" className={"mx-4 rounded text-white px-3 " + (page === "Send" && navSecondary)}>
-                        SEND
+                    <a onClick={setPageNum(pageNum)} href={PageName[pageNum].reference} className={"basis-1/3 rounded text-white px-3 " + (page === PageName[pageNum].name && navSecondary)}>
+                        Forward
                     </a>
                 </div>
-            </div>
         </nav>
     )
 }
--- pages/tabs/auto.js
+++ pages/tabs/auto.js
@@ -10,25 +10,28 @@ export default function Auto() {
         <>
             <Navbar page="Auto" />
             <div className="flex justify-center py-4">
-                <button className={"w-5/6 py-2 rounded text-white text-3xl font-bold background-color:bg-emerald-600 " + (getData("auto", "var1") ? "bg-gray-400 active:bg-emerald-600" : "bg-emerald-600 active:bg-gray-400")} onClick={() => {setData("auto", "var1", !getData("auto", "var1")); forceUpdate()}}>Robot Left the Tarmac</button>
+                <button className={"w-5/6 py-2 rounded text-white text-3xl font-bold background-color:bg-emerald-600 " + (getData("auto", "var1") ? "bg-gray-400 active:bg-emerald-600" : "bg-emerald-600 active:bg-gray-400")} onClick={() => {setData("auto", "var1", !getData("auto", "var1")); forceUpdate()}}>Robot Left the Community</button>
             </div>
-            <h1 className="flex justify-center py-2 text-white text-3xl font-bold">Low Hub Scored</h1>
+            <h1 className="flex justify-center py-2 text-white text-3xl font-bold">Pieces scored on high nodes</h1>
             <div className="flex flex-row justify-evenly pb-5">
                 <button className="basis-1/4 rounded-full bg-emerald-600 text-white text-7xl font-bold background-color:bg-emerald-600 active:bg-gray-400" onClick={() => {setData("auto", "var2", getData("auto", "var2") - 1); forceUpdate()}}>-</button>
                 <h1 className="basis-1/5 text-center text-white text-8xl font-bold">{getData("auto", "var2")}</h1>
                 <button className="basis-1/4 rounded-full bg-emerald-600 text-white text-7xl font-bold background-color:bg-emerald-600 active:bg-gray-400" onClick={() => {setData("auto", "var2", getData("auto", "var2") + 1); forceUpdate()}}>+</button>
             </div>
-            <h1 className="flex justify-center py-2 text-white text-3xl font-bold">High Hub Scored</h1>
+            <h1 className="flex justify-center py-2 text-white text-3xl font-bold">Pieces scored on middle nodes</h1>
             <div className="flex flex-row justify-evenly pb-5">
                 <button className="basis-1/4 rounded-full bg-emerald-600 text-white text-7xl font-bold background-color:bg-emerald-600 active:bg-gray-400" onClick={() => {setData("auto", "var3", getData("auto", "var3") - 1); forceUpdate()}}>-</button>
                 <h1 className="basis-1/5 text-center text-white text-8xl font-bold">{getData("auto", "var3")}</h1>
                 <button className="basis-1/4 rounded-full bg-emerald-600 text-white text-7xl font-bold background-color:bg-emerald-600 active:bg-gray-400" onClick={() => {setData("auto", "var3", getData("auto", "var3") + 1); forceUpdate()}}>+</button>
             </div>
-            <h1 className="flex justify-center py-2 text-white text-3xl font-bold">Shots Missed</h1>
+            <h1 className="flex justify-center py-2 text-white text-3xl font-bold">Pieces scored on hybrid nodes</h1>
             <div className="flex flex-row justify-evenly pb-5">
                 <button className="basis-1/4 rounded-full bg-emerald-600 text-white text-7xl font-bold background-color:bg-emerald-600 active:bg-gray-400" onClick={() => {setData("auto", "var4", getData("auto", "var4") - 1); forceUpdate()}}>-</button>
                 <h1 className="basis-1/5 text-center text-white text-8xl font-bold">{getData("auto", "var4")}</h1>
                 <button className="basis-1/4 rounded-full bg-emerald-600 text-white text-7xl font-bold background-color:bg-emerald-600 active:bg-gray-400" onClick={() => {setData("auto", "var4", getData("auto", "var4") + 1); forceUpdate()}}>+</button>
+            </div>
+            <div className="flex flex-row justify-evenly py-4">
+                <button className={"w-5/6 py-2 rounded text-white text-3xl font-bold background-color:bg-emerald-600 " + (getData("auto", "var5") ? "bg-gray-400 active:bg-emerald-600" : "bg-emerald-600 active:bg-gray-400")} onClick={() => {setData("auto", "var1", !getData("auto", "var5")); forceUpdate()}}>Robot was Engaged</button>
             </div>
         </>
     )
