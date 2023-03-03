e8b2662e6f38127dba42789985105ae4e9780188
Rowan Eklund <717538@dpsk12.net>
2023-03-02 17:44:03 -0700

    feat(): Updated node packages

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,14 @@ import Head from 'next/head'
 import Navbar from "../components/Navbar";
 
 export default function Home() {
-  return (
-    <div>
-        <Navbar />
-        <Head>
-            <title>1410 Match Scouting</title>
-            <meta name="description" content="A data collection app used by the frc 1410 scouting sub-team" />
-            <link rel="icon" href="./favicon.png" />
-        </Head>
+    return (
+            <div>
+                <Head>
+                    <title>1410 Match Scouting</title>
+                    <meta name="description" content="A data collection app used by the frc 1410 scouting sub-team" />
+                    <link rel="icon" href="./favicon.png" />
+                </Head>
+                <a href="/tabs/pregame" className="flex items-center justify-center basis-1/12 rounded-full bg-emerald-600 text-white text-3xl font-bold background-color:bg-emerald-600 active:bg-gray-400 p-8">Data Time!</a>
     </div>
   )
 }
