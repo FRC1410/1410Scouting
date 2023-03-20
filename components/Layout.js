87ec13b5558c3feb3de11804fb25ed004b4cd5c6
Rowan Eklund <717538@dpsk12.net>
2023-03-20 16:55:55 -0600

    added back footer

--- components/Layout.js
+++ components/Layout.js
@@ -4,6 +4,9 @@ export default function Layout({ children }) {
     return (
         <div className='layout'>
             { children }
+            <footer>
+                <p className="p-1">Created By: Rowan Eklund and Owen Duffy</p>
+            </footer>
         </div>
     )
 }
