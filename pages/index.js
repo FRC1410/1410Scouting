b68907b704b36d5df991a759715a4103925100ec
Rowan Eklund <717538@dpsk12.net>
2023-03-20 16:46:38 -0600

    chore(Color): Activation color changed from gray to dark green

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
                     <meta name="description" content="A data collection app used by the frc 1410 scouting sub-team" />
                     <link rel="icon" href="./favicon.png" />
                 </Head>
-                <a href="/tabs/pregame" className="flex items-center justify-center basis-1/12 rounded-full bg-emerald-600 text-white text-3xl font-bold background-color:bg-emerald-600 active:bg-gray-400 p-8">Data Time!</a>
+                <a href="/tabs/pregame" className="flex items-center justify-center basis-1/12 rounded-full bg-emerald-600 text-white text-3xl font-bold background-color:bg-emerald-600 active:bg-emerald-900 p-8">Data Time!</a>
     </div>
   )
 }
--- pages/tabs/auto.js
+++ pages/tabs/auto.js
@@ -10,7 +10,7 @@ export default function Auto() {
         <>
         <Navbar pageNum={2} />
             <div className="flex justify-center">
-                <button className={"w-5/6 rounded text-white text-2xl font-bold " + (getData("auto", "Robot Left Community?") ? "bg-gray-400 active:bg-emerald-600" : "bg-emerald-600 active:bg-gray-400")} onClick={() => {setData("auto", "Robot Left Community?", !getData("auto", "Robot Left Community?")); forceUpdate()}}>Robot Left the Community</button>
+                <button className={"w-5/6 rounded text-white text-2xl font-bold " + (getData("auto", "Robot Left Community?") ? "bg-emerald-900 active:bg-emerald-600" : "bg-emerald-600 active:bg-emerald-900")} onClick={() => {setData("auto", "Robot Left Community?", !getData("auto", "Robot Left Community?")); forceUpdate()}}>Robot Left the Community</button>
             </div>
             <div className="flex flex-row justify-evenly">
                 <h1 className="basis-1/2 text-center text-white text-2xl font-bold">Cones</h1>
@@ -20,34 +20,34 @@ export default function Auto() {
                 <h1 className="flex text-center justify-center text-white text-2xl font-bold">High</h1>
             </div>
             <div className="flex flex-row justify-evenly">
-                <button className="basis-1/12 rounded-full bg-emerald-600 text-white text-4xl font-bold background-color:bg-emerald-600 active:bg-gray-400" onClick={() => {setData("auto", "High Cone Auto", getData("auto", "High Cone Auto") - 1); forceUpdate()}}>-</button>
+                <button className="basis-1/12 rounded-full bg-emerald-600 text-white text-4xl font-bold background-color:bg-emerald-600 active:bg-emerald-900" onClick={() => {setData("auto", "High Cone Auto", getData("auto", "High Cone Auto") - 1); forceUpdate()}}>-</button>
                 <h1 className="basis-1/12 text-center py-4 text-white text-3xl font-bold">{getData("auto", "High Cone Auto")}</h1>
-                <button className="basis-1/12 rounded-full bg-emerald-600 text-white text-3xl font-bold background-color:bg-emerald-600 active:bg-gray-400" onClick={() => {setData("auto", "High Cone Auto", getData("auto", "High Cone Auto") + 1); forceUpdate()}}>+</button>
-                <button className="basis-1/12 rounded-full bg-emerald-600 text-white text-3xl font-bold background-color:bg-emerald-600 active:bg-gray-400" onClick={() => {setData("auto", "High Cube Auto", getData("auto", "High Cube Auto") - 1); forceUpdate()}}>-</button>
+                <button className="basis-1/12 rounded-full bg-emerald-600 text-white text-3xl font-bold background-color:bg-emerald-600 active:bg-emerald-900" onClick={() => {setData("auto", "High Cone Auto", getData("auto", "High Cone Auto") + 1); forceUpdate()}}>+</button>
+                <button className="basis-1/12 rounded-full bg-emerald-600 text-white text-3xl font-bold background-color:bg-emerald-600 active:bg-emerald-900" onClick={() => {setData("auto", "High Cube Auto", getData("auto", "High Cube Auto") - 1); forceUpdate()}}>-</button>
                 <h1 className="basis-1/12 text-center py-4 text-white text-3xl font-bold">{getData("auto", "High Cube Auto")}</h1>
-                <button className="basis-1/12 rounded-full bg-emerald-600 text-white text-3xl font-bold background-color:bg-emerald-600 active:bg-gray-400" onClick={() => {setData("auto", "High Cube Auto", getData("auto", "High Cube Auto") + 1); forceUpdate()}}>+</button>
+                <button className="basis-1/12 rounded-full bg-emerald-600 text-white text-3xl font-bold background-color:bg-emerald-600 active:bg-emerald-900" onClick={() => {setData("auto", "High Cube Auto", getData("auto", "High Cube Auto") + 1); forceUpdate()}}>+</button>
             </div>
             <h1 className="flex text-center justify-center text-white text-2xl font-bold">Mid</h1>
             <div className="flex flex-row justify-evenly">
-                <button className="basis-1/12 rounded-full bg-emerald-600 text-white text-3xl font-bold background-color:bg-emerald-600 active:bg-gray-400" onClick={() => {setData("auto", "Mid Cone Auto", getData("auto", "Mid Cone Auto") - 1); forceUpdate()}}>-</button>
+                <button className="basis-1/12 rounded-full bg-emerald-600 text-white text-3xl font-bold background-color:bg-emerald-600 active:bg-emerald-900" onClick={() => {setData("auto", "Mid Cone Auto", getData("auto", "Mid Cone Auto") - 1); forceUpdate()}}>-</button>
                 <h1 className="basis-1/12 text-center py-4 text-white text-3xl font-bold">{getData("auto", "Mid Cone Auto")}</h1>
-                <button className="basis-1/12 rounded-full bg-emerald-600 text-white text-3xl font-bold background-color:bg-emerald-600 active:bg-gray-400" onClick={() => {setData("auto", "Mid Cone Auto", getData("auto", "Mid Cone Auto") + 1); forceUpdate()}}>+</button>
-                <button className="basis-1/12 rounded-full bg-emerald-600 text-white text-3xl font-bold background-color:bg-emerald-600 active:bg-gray-400" onClick={() => {setData("auto", "Mid Cube Auto", getData("auto", "Mid Cube Auto") - 1); forceUpdate()}}>-</button>
+                <button className="basis-1/12 rounded-full bg-emerald-600 text-white text-3xl font-bold background-color:bg-emerald-600 active:bg-emerald-900" onClick={() => {setData("auto", "Mid Cone Auto", getData("auto", "Mid Cone Auto") + 1); forceUpdate()}}>+</button>
+                <button className="basis-1/12 rounded-full bg-emerald-600 text-white text-3xl font-bold background-color:bg-emerald-600 active:bg-emerald-900" onClick={() => {setData("auto", "Mid Cube Auto", getData("auto", "Mid Cube Auto") - 1); forceUpdate()}}>-</button>
                 <h1 className="basis-1/12 text-center py-4 text-white text-3xl font-bold">{getData("auto", "Mid Cube Auto")}</h1>
-                <button className="basis-1/12 rounded-full bg-emerald-600 text-white text-3xl font-bold background-color:bg-emerald-600 active:bg-gray-400" onClick={() => {setData("auto", "Mid Cube Auto", getData("auto", "Mid Cube Auto") + 1); forceUpdate()}}>+</button>
+                <button className="basis-1/12 rounded-full bg-emerald-600 text-white text-3xl font-bold background-color:bg-emerald-600 active:bg-emerald-900" onClick={() => {setData("auto", "Mid Cube Auto", getData("auto", "Mid Cube Auto") + 1); forceUpdate()}}>+</button>
             </div>
             <h1 className="flex text-center justify-center text-white text-2xl font-bold">Hybrid</h1>
             <div className="flex flex-row justify-evenly">
-                <button className="basis-1/12 rounded-full bg-emerald-600 text-white text-3xl font-bold background-color:bg-emerald-600 active:bg-gray-400" onClick={() => {setData("auto", "Hybrid Cone Auto", getData("auto", "Hybrid Cone Auto") - 1); forceUpdate()}}>-</button>
+                <button className="basis-1/12 rounded-full bg-emerald-600 text-white text-3xl font-bold background-color:bg-emerald-600 active:bg-emerald-900" onClick={() => {setData("auto", "Hybrid Cone Auto", getData("auto", "Hybrid Cone Auto") - 1); forceUpdate()}}>-</button>
                 <h1 className="basis-1/12 text-center py-4 text-white text-3xl font-bold">{getData("auto", "Hybrid Cone Auto")}</h1>
-                <button className="basis-1/12 rounded-full bg-emerald-600 text-white text-3xl font-bold background-color:bg-emerald-600 active:bg-gray-400" onClick={() => {setData("auto", "Hybrid Cone Auto", getData("auto", "Hybrid Cone Auto") + 1); forceUpdate()}}>+</button>
-                <button className="basis-1/12 rounded-full bg-emerald-600 text-white text-3xl font-bold background-color:bg-emerald-600 active:bg-gray-400" onClick={() => {setData("auto", "Hybrid Cube Auto", getData("auto", "Hybrid Cube Auto") - 1); forceUpdate()}}>-</button>
+                <button className="basis-1/12 rounded-full bg-emerald-600 text-white text-3xl font-bold background-color:bg-emerald-600 active:bg-emerald-900" onClick={() => {setData("auto", "Hybrid Cone Auto", getData("auto", "Hybrid Cone Auto") + 1); forceUpdate()}}>+</button>
+                <button className="basis-1/12 rounded-full bg-emerald-600 text-white text-3xl font-bold background-color:bg-emerald-600 active:bg-emerald-900" onClick={() => {setData("auto", "Hybrid Cube Auto", getData("auto", "Hybrid Cube Auto") - 1); forceUpdate()}}>-</button>
                 <h1 className="basis-1/12 text-center py-4 text-white text-3xl font-bold">{getData("auto", "Hybrid Cube Auto")}</h1>
-                <button className="basis-1/12 rounded-full bg-emerald-600 text-white text-3xl font-bold background-color:bg-emerald-600 active:bg-gray-400" onClick={() => {setData("auto", "Hybrid Cube Auto", getData("auto", "Hybrid Cube Auto") + 1); forceUpdate()}}>+</button>
+                <button className="basis-1/12 rounded-full bg-emerald-600 text-white text-3xl font-bold background-color:bg-emerald-600 active:bg-emerald-900" onClick={() => {setData("auto", "Hybrid Cube Auto", getData("auto", "Hybrid Cube Auto") + 1); forceUpdate()}}>+</button>
             </div>
             <div className="flex flex-row justify-evenly pt-1">
-                <button className={"w-5/12 rounded text-white text-2xl font-bold " + (getData("auto", "Docked Auto") ? "bg-gray-400 active:bg-emerald-600" : "bg-emerald-600 active:bg-gray-400")} onClick={() => {setData("auto", "Docked Auto", !getData("auto", "Docked Auto")); forceUpdate()}}>Did they Dock?</button>
-                <button className={"w-5/12 rounded text-white text-2xl font-bold " + (getData("auto", "Engaged Auto") ? "bg-gray-400 active:bg-emerald-600" : "bg-emerald-600 active:bg-gray-400")} onClick={() => {setData("auto", "Engaged Auto", !getData("auto", "Engaged Auto")); forceUpdate()}}>Did they Engage?</button>
+                <button className={"w-5/12 rounded text-white text-2xl font-bold " + (getData("auto", "Docked Auto") ? "bg-emerald-900 active:bg-emerald-600" : "bg-emerald-600 active:bg-emerald-900")} onClick={() => {setData("auto", "Docked Auto", !getData("auto", "Docked Auto")); forceUpdate()}}>Did they Dock?</button>
+                <button className={"w-5/12 rounded text-white text-2xl font-bold " + (getData("auto", "Engaged Auto") ? "bg-emerald-900 active:bg-emerald-600" : "bg-emerald-600 active:bg-emerald-900")} onClick={() => {setData("auto", "Engaged Auto", !getData("auto", "Engaged Auto")); forceUpdate()}}>Did they Engage?</button>
             </div>
         </>
     )
